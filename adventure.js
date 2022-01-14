d6d51b81039c474e3904cb97c36b81db69506a83
kaserrr <99057099@mydavinci.nl>
2022-01-14 11:39:35 +0100

    Initial commit

---
+++ adventure.js
@@ -0,0 +1,4 @@
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const buttons = document.getElementById("game-buttons");
+const inventoryItem = document.getElementById("inventoryItem");
